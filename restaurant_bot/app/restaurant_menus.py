2551dbc1fe5083c8a3b3ea6d1029a183f7c97a38
Ilia <ilyarus199939@gmail.com>
2024-10-29 02:57:31 +0600

    Upd #47 (upd docstrings.)

--- restaurant_bot/app/restaurant_menus.py
+++ restaurant_bot/app/restaurant_menus.py
@@ -10,15 +10,16 @@ back_button = lambda lang_code: types.InlineKeyboardButton(
 
 
 def edit_dish_menu(lang_code: str, msg: DBInterface) -> types.InlineKeyboardMarkup:
-    """
+    """Compose "edit dish" menu in Restaurant's language.
 
     Args:
-        lang_code:
-        msg:
+        lang_code: Language code of the Restaurant's language.
+        msg: DBInterface instance containing message from User.
 
     Returns:
+        "Edit dish" menu in Restaurant's language.
 
-    """  # TODO
+    """
     menu = types.InlineKeyboardMarkup(row_width=1)
     for dish in msg.get_dishes():
         dish_button = types.InlineKeyboardButton(text=dish[1], callback_data=dish[0])
@@ -28,15 +29,17 @@ def edit_dish_menu(lang_code: str, msg: DBInterface) -> types.InlineKeyboardMark
 
 
 def edit_dish_param_menu(lang_code: str, callback: DBInterface) -> types.InlineKeyboardMarkup:
-    """
+    """Compose "edit dish parameter" menu in Restaurant's language.
     
     Args:
-        lang_code: 
-        callback: 
+        lang_code: Language code of the Restaurant's language.
+        callback: DBInterface instance containing callback query from
+            User.
 
     Returns:
+        "Edit dish parameter" menu in Restaurant's language.
 
-    """  # TODO
+    """
     cat_button = types.InlineKeyboardButton(
         text=texts[lang_code]["EDIT_CAT_BTN"],
         callback_data=f"cat {callback.data_to_read.data}"
@@ -83,15 +86,16 @@ def lang_sel_menu(lang_code: str) -> types.InlineKeyboardMarkup:
 
 # Courier accept order menu.
 def courier_accept_menu(order_uuid: str, lang_code: str) -> types.InlineKeyboardMarkup:
-    """
+    """Compose "accept order" in courier's language.
 
     Args:
-        order_uuid:
-        lang_code:
+        order_uuid: Order UUID.
+        lang_code: Language code of courier's language.
 
     Returns:
+        "Accept order" menu in required language.
 
-    """  # TODO
+    """
     menu = types.InlineKeyboardMarkup(row_width=1)
     accept_button = types.InlineKeyboardButton(
         text=texts[lang_code]["COURIER_ACCEPT_BTN"],
@@ -103,15 +107,16 @@ def courier_accept_menu(order_uuid: str, lang_code: str) -> types.InlineKeyboard
 
 # Courier order in delivery menu.
 def courier_in_delivery_menu(order_uuid: str, courier_lang: str) -> types.InlineKeyboardMarkup:
-    """
+    """Compose "order in delivery" menu in courier's language.
 
     Args:
-        order_uuid:
-        courier_lang:
+        order_uuid: Order UUID.
+        courier_lang: Language code of courier's language.
 
     Returns:
+        "Order in delivery" menu in required language.
 
-    """  # TODO
+    """
     menu = types.InlineKeyboardMarkup(row_width=1)
     order_handled_button = types.InlineKeyboardButton(
         text=texts[courier_lang]["COUR_ORDER_IN_DELIVERY_BTN"],
